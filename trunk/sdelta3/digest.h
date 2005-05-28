bbd0bc62f68863545e2c4e8f4f97c17092cafcaa
svinn <svinn@c6675f70-c4e4-0310-b883-b9790b8e7a1a>
2005-05-28 21:14:35 +0000

    use libgcrypt instead of gnutls

--- trunk/sdelta3/digest.h
+++ trunk/sdelta3/digest.h
@@ -4,20 +4,11 @@
 /* SHA1 digest */
 #define DIGEST_SIZE 20
 
-#if defined(USE_GNUTLS10) || defined(USE_GNUTLS)
-#include <gnutls/gnutls.h>
-#ifdef USE_GNUTLS10
-typedef gnutls_datum gnutls_datum_t;
-#endif
-#define DECLARE_DIGEST_VARS \
-    gnutls_datum_t datum; \
-    size_t digest_buf_size
-#define GET_DIGEST(buf, sz, res) do { \
-    datum.data = (buf); \
-    datum.size = (sz); \
-    digest_buf_size = DIGEST_SIZE; \
-    gnutls_fingerprint(GNUTLS_DIG_SHA, &datum, (res), &digest_buf_size); \
-} while(0)
+#ifdef USE_LIBGCRYPT
+#include <gcrypt.h>
+#define DECLARE_DIGEST_VARS
+#define GET_DIGEST(buf, sz, res) \
+    gcry_md_hash_buffer(GCRY_MD_SHA1, (res), (buf), (sz))
 #else /* OpenSSL or libmd */
 #ifdef USE_LIBMD
 #include <sha.h>
